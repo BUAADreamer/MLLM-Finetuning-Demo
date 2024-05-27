e314f0abca4fc8bbae2d18a5779a5f25e3320942
BUAADreamer <1428195643@qq.com>
2024-05-27 20:04:59 +0800

    update pics and pt_demo data

--- upload_dataset.py
+++ upload_dataset.py
@@ -1,35 +1,41 @@
 import json
 
-with open("data/mllm_demo.json") as f:
-    examples = json.loads(f.read())
-
-import huggingface_hub
-
-huggingface_hub.login("hf_xxxxx")  # 替换为你自己的key从而登录hf
-
-from datasets import Dataset, Features, Image, Sequence, Value
+import fire
 
 
-def gen():
-    for data in examples:
-        yield data
+def main(hf_token="hf_xxxxx", json_file="data/mllm_demo.json", hf_data_id="BUAADreamer/mllm_demo"):
+    with open(json_file) as f:
+        examples = json.loads(f.read())
+
+    import huggingface_hub
+
+    huggingface_hub.login(hf_token)  # 替换为你自己的key从而登录hf
+
+    from datasets import Dataset, Features, Image, Sequence, Value
+
+    def gen():
+        for data in examples:
+            yield data
+
+    # 构建数据集feature
+    features = Features(
+        {
+            'messages': [
+                {
+                    'role': Value(dtype='string', id=None),
+                    'content': Value(dtype='string', id=None),
+                }
+            ],
+            'images': Sequence(feature=Image(decode=True, id=None), length=-1, id=None),
+        }
+    )
+
+    # 使用迭代生成
+    dataset = Dataset.from_generator(gen, features=features)
+
+    # push到huggingface
+    dataset.push_to_hub(hf_data_id)
 
 
-# 构建数据集feature
-features = Features(
-    {
-        'messages': [
-            {
-                'role': Value(dtype='string', id=None),
-                'content': Value(dtype='string', id=None),
-            }
-        ],
-        'images': Sequence(feature=Image(decode=True, id=None), length=-1, id=None),
-    }
-)
-
-# 使用迭代生成
-dataset = Dataset.from_generator(gen, features=features)
-
-# push到huggingface
-dataset.push_to_hub("BUAADreamer/mllm_demo")
+if __name__ == '__main__':
+    fire.Fire(main)
